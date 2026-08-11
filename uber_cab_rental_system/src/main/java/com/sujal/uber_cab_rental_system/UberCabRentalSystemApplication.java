006999b60d16e49c9474bc34c7250e1679cad102
Sujal Sharma <sujalsharma9602@gmail.com>
2026-08-12 00:03:11 +0530

    Basic Code

---
+++ uber_cab_rental_system/src/main/java/com/sujal/uber_cab_rental_system/UberCabRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sujal.uber_cab_rental_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberCabRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberCabRentalSystemApplication.class, args);
+	}
+
+}
---
+++ uber_cab_rental_system/src/test/java/com/sujal/uber_cab_rental_system/UberCabRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujal.uber_cab_rental_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberCabRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
